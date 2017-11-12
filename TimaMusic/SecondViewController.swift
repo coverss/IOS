50ab60f6db2247b3d8c60d53ee2312656036d352
sandybekn <sandybekn@gmail.com>
2017-11-12 18:42:31 +0600

    app can run the music

---
+++ TimaMusic/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TimaMusic
+//
+//  Created by Tima on 12.11.17.
+//  Copyright © 2017 Tima. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
